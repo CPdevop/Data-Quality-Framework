24c6ea3a0d1e0f810109b4d894f26f343c26bf52
CPdevop <116315533+CPdevop@users.noreply.github.com>
2025-11-01 01:15:39 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,5 +2,5 @@ import os
 from sqlalchemy import create_engine
 
 def get_engine():
-    url = os.getenv("DQ_DB_URL", "postgresql+psycopg2://postgres:1234@localhost:5432/synthetic_bank")
+    url = os.getenv("DQ_DB_URL", "postgresql+psycopg2://postgres:postgres@localhost:5432/synthetic_bank")
     return create_engine(url)
